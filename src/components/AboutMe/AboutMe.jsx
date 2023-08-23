8640f6e2a73ffdba205f17189ff71393da96006d
Alexander Belov <marlo-nn@yandex.ru>
2023-08-24 00:04:29 +0300

    🔠 Задачи по врестке Main  Techs AboutMe

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -4,11 +4,72 @@
 
 import React from 'react';
 import './AboutMe.css';
+import portret from '../../images/foto_title.jpg'
 
 const AboutMe = () => {
   return (
     <section className='student' id='student'>
-      <b>AboutMe</b> — компонент с информацией о студенте.
+      {/* <b>AboutMe</b> — компонент с информацией о студенте. */}
+      <h2 className="student__title">Студент</h2>
+      <div className="student__line"></div>
+      <ul className='student__container'>
+        <li className="student__wrapper student__wrapper_info">
+          <h3 className="student__name">Виталий</h3>
+          <h4 className="student__about">Фронтенд-разработчик, 30 лет</h4>
+          <p className="student__description">Я&nbsp;родился и&nbsp;живу в&nbsp;Саратове, закончил факультет экономики СГУ. У&nbsp;меня есть жена и&nbsp;дочь. Я&nbsp;люблю слушать музыку, а&nbsp;ещё увлекаюсь бегом. Недавно начал кодить. С&nbsp;2015 года работал в&nbsp;компании &laquo;СКБ Контур&raquo;. После того, как прошёл курс по&nbsp;веб-разработке, начал заниматься фриланс-заказами и&nbsp;ушёл с&nbsp;постоянной работы.</p>
+
+          <ul className='student__contacts'>
+            <li className='student__contact'>
+              <a href="#" className="student__link links__hover">
+                Github
+              </a>
+            </li>
+            <li className='student__contact'>
+              <a href="#" className="student__link links__hover">
+                Vkontakte
+              </a>
+            </li>
+            <li className='student__contact'>
+              <a href="#" className="student__link links__hover">
+                Instagram
+              </a>
+            </li>
+          </ul>
+
+        </li>
+        <li className="student__wrapper">
+          <img src={portret} alt="!" className='student__portret' />
+        </li>
+      </ul>
+
+
+      <h2 className="student__title student__title_portfolio">Портфолио</h2>
+      <ul className='student__list-portfolio'>
+        <li className='student__item-portfolio'>
+          <a href="" className="student__link-portfolio links__hover">
+            Статичный сайт
+            <span className="student__arrow-portfolio">
+              ↗
+            </span>
+          </a>
+        </li>
+        <li className='student__item-portfolio'>
+          <a href="" className="student__link-portfolio links__hover">
+            Адаптивный сайт
+            <span className="student__arrow-portfolio">
+              ↗
+            </span>
+          </a>
+        </li>
+        <li className='student__item-portfolio'>
+          <a href="" className="student__link-portfolio links__hover">
+            Одностраничное приложение
+            <span className="student__arrow-portfolio">
+              ↗
+            </span>
+          </a>
+        </li>
+      </ul>
     </section>
   );
 }
--- src/components/AboutProject/AboutProject.jsx
+++ src/components/AboutProject/AboutProject.jsx
@@ -5,15 +5,15 @@ import './AboutProject.css';
 const AboutProject = () => {
   return (
     <section className='about' id='about'>
-      <h1 className="about__title">О проекте</h1>
+      <h2 className="about__title">О проекте</h2>
       <div className='about__line'></div>
       <ul className='about__list'>
         <li className='about_item'>
-          <h2 className='about__subtitle'>Дипломный проект включал 5 этапов</h2>
+          <h3 className='about__subtitle'>Дипломный проект включал 5 этапов</h3>
           <p className='about__paragraph'>Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
         </li>
         <li className='about_item'>
-          <h2 className='about__subtitle'>На выполнение диплома ушло 5 недель</h2>
+          <h3 className='about__subtitle'>На выполнение диплома ушло 5 недель</h3>
           <p className='about__paragraph'>У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
         </li>
       </ul>
