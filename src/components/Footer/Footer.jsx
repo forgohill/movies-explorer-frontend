09fcca7c689830ab815a0369a4d45b5133093a19
Alexander Belov <marlo-nn@yandex.ru>
2023-08-24 10:29:48 +0300

    🔠 Задачи по врестке Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
+import './Footer.css';
+
 const Footer = () => {
   return (
-    <div>
-      <b>Footer</b> — презентационный компонент, который отрисовывает подвал.
-    </div>
+    <footer className='footer'>
+      <h3 className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</h3>
+      <div className="footer__line"></div>
+      <div className="footer__container">
+        <p className="footer__text footer__text_copyright">© 2020</p>
+        <div className="footer__wrapper">
+          <p className="footer__text">Яндекс.Практикум</p>
+          <a href="#" className="footer__link">Github</a>
+        </div>
+      </div>
+    </footer>
   );
 }
 
