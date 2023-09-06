6acb3f8c8c63a0c93bde879d9ac6175b5ba9af43
Alexander Belov <marlo-nn@yandex.ru>
2023-09-06 10:08:13 +0300

    🐞 перенос в папку App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -15,18 +15,18 @@
 
 import React, { useState } from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
-import { AuthorizedContext } from '../contexts/AuthorizedContext';
+import { AuthorizedContext } from '../../contexts/AuthorizedContext';
 
 import './App.css';
-import Header from './Header/Header';
-import Main from './Main/Main';
-import Movies from './Movies/Movies';
-import SavedMovies from './SavedMovies/SavedMovies';
-import Register from './Register/Register';
-import Login from './Login/Login';
-import Profile from './Profile/Profile';
-import Footer from './Footer/Footer';
-import PageNotFound from './PageNotFound/PageNotFound';
+import Header from '../Header/Header';
+import Main from '../Main/Main';
+import Movies from '../Movies/Movies';
+import SavedMovies from '../SavedMovies/SavedMovies';
+import Register from '../Register/Register';
+import Login from '../Login/Login';
+import Profile from '../Profile/Profile';
+import Footer from '../Footer/Footer';
+import PageNotFound from '../PageNotFound/PageNotFound';
 
 function App() {
 
