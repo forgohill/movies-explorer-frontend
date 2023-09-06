0784d2c186edd7741362e3ad8151ad39f7a1e777
Alexander Belov <marlo-nn@yandex.ru>
2023-09-06 15:27:33 +0300

    🐞переделал PageNotFound Auth Profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -17,66 +17,68 @@ const Profile = ({ onAuth }) => {
   return (
     <main
       className='profile'>
-      <h1 className="profile__title">Привет, Виталий!</h1>
+      <div className="profile__container">
+        <h1 className="profile__title">Привет, Виталий!</h1>
+        {/* <form className={`profile__form ${isVisible === false ? 'profile__form_show' : ''}`}> */}
+        <form className='profile__form'>
+          <label
+            htmlFor="email"
+            className='profile__label'>
+            Имя
+            <input
+              placeholder='Введите имя'
+              // value={'Виталий'}
+              disabled={isVisible}
+              type="text"
+              id='email'
+              className='profile__input' />
+            <span
+              className='profile__error'>
+              Пожалуйста, используйте не менее 4 символов (сейчас вы используете 3 символов).
+            </span>
+          </label>
 
-      <form className='profile__form'>
-        <label
-          htmlFor="email"
-          className='profile__label'>
-          Имя
-          <input
-            placeholder='Введите имя'
-            // value={'Виталий'}
-            disabled={isVisible}
-            type="text"
-            id='email'
-            className='profile__input' />
+          <div className="profile__line"></div>
+
+          <label
+            htmlFor="name"
+            className='profile__label'>
+            E-mail
+            <input
+              disabled={isVisible}
+              // value={'pochta@yandex.ru'}
+              placeholder='Введите e-mail'
+              type="text"
+              id='name'
+              className='profile__input' />
+            <span
+              className='profile__error'>
+              Пожалуйста, используйте не менее 4 символов (сейчас вы используете 3 символов).
+            </span>
+          </label>
           <span
-            className='profile__error'>
-            Пожалуйста, используйте не менее 4 символов (сейчас вы используете 3 символов).
+            className={`profile__error-submit ${isVisible === false ? 'profile__error-submit_show' : ''}`}>
+            При обновлении профиля произошла ошибка.
           </span>
-        </label>
-
-        <div className="profile__line"></div>
-
-        <label
-          htmlFor="name"
-          className='profile__label'>
-          E-mail
-          <input
-            disabled={isVisible}
-            // value={'pochta@yandex.ru'}
-            placeholder='Введите e-mail'
-            type="text"
-            id='name'
-            className='profile__input' />
-          <span
-            className='profile__error'>
-            Пожалуйста, используйте не менее 4 символов (сейчас вы используете 3 символов).
-          </span>
-        </label>
-        <span
-          className={`profile__error-submit ${isVisible === false ? 'profile__error-submit_show' : ''}`}>
-          При обновлении профиля произошла ошибка.
-        </span>
+          <button
+            // disabled
+            onClick={handleRedact}
+            className={`profile__btn-save  ${isVisible === false ? 'profile__btn-save_show' : ''}`}
+          >Сохранить</button>
+        </form>
         <button
-          // disabled
           onClick={handleRedact}
-          className={`profile__btn-save  ${isVisible === false ? 'profile__btn-save_show' : ''}`}
-        >Сохранить</button>
-      </form>
-      <button
-        onClick={handleRedact}
-        className={`profile__btn-redact ${isVisible === true ? 'profile__btn-redact_show' : ''}`}
-      >Редактировать</button>
+          className={`profile__btn-redact ${isVisible === true ? 'profile__btn-redact_show' : ''}`}
+        >Редактировать</button>
 
-      <Link
-        to='/'
-        className={`profile__link ${isVisible === true ? 'profile__link_show' : ''}`}
-        onClick={onAuth}
-      >
-        Выйти из аккаунта
-      </Link>
+        <Link
+          to='/'
+          className={`profile__link ${isVisible === true ? 'profile__link_show' : ''}`}
+          onClick={onAuth}
+        >
+          Выйти из аккаунта
+        </Link>
+      </div>
     </main >
   );
 }
